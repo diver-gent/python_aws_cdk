9ef7d2271f909ab62489811ad6784d1386123f02
btowns <bill.townsley@nike.com>
2020-06-27 15:37:48 -0700

    saving state

---
+++ app.py
@@ -0,0 +1,28 @@
+from aws_cdk import core
+
+from my_custom_resource import MyCustomResource
+
+
+# A Stack that sets up MyCustomResource and shows how to get an
+# attribute from it.
+
+class MyStack(core.Stack):
+    def __init__(self, scope: core.App, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        resource = MyCustomResource(
+            self, "python_aws_cdk_resource",
+            message="python_aws_cdk_resource says hello",
+        )
+
+        # Publish the custom resource output
+        core.CfnOutput(
+            self, "ResponseMessage",
+            description="This is a message from the python_aws_cdk Custom Resource",
+            value=resource.response,
+        )
+
+
+app = core.App()
+MyStack(app, "python-aws-cdk")
+app.synth()
